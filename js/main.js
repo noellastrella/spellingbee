c141f5d4a0e01c6adcbc05829c057758bacf28bd
Noel Lastrella <noel.lastrella@gmail.com>
2024-09-21 18:30:49 -0400

    added

--- js/main.js
+++ js/main.js
@@ -1,32 +1,25 @@
-//using lodash
-
-
-
 var words;
 var letters = [];
+var guessArray = []
+var guess = "";
 
-// Make the fetch request with the provided options
 fetch('/js/wordlist/index.json')
   .then(response => {
-    // Check if the request was successful
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    // Parse the response as JSON
     return response.json();
   })
   .then(data => {
-    // Handle the JSON data
     words = data.reduce((acc,curr)=>{
         curr.length > 3 && acc.push(curr)
         return acc;
     }, [])
-
+    
+    console.log(data.length, data);
     init();
-    console.log(data);
   })
   .catch(error => {
-    // Handle any errors that occurred during the fetch
     console.error('Fetch error:', error);
   });
 
@@ -34,11 +27,9 @@ function init(){
     let vowels = "AEIOU".split("");
     let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     let consonants = "BCDFGHJKLMNPQRSTVWXYZ".split("");
+    let lettersJoined;
 
     let letters = [];
-    
-    //letters.push(_.sample(vowels));  //lodash
-    //letters.push(_.sampleSize(alphabet,6)); //lodash
 
     letters.push(
         [...vowels
@@ -48,24 +39,52 @@ function init(){
     );
 
     letters.push(
-        [...alphabet
+        [...consonants
             .map(value => ({ value, sort: Math.random() }))
             .sort((a, b) => a.sort - b.sort)
             .map(({ value }) => value).splice(0,6) ]
     );
 
-    letters = letters.flat();
+    letters = ["A", "A", "H", "E", "D"]
+
+    lettersJoined = letters.join("").toLowerCase();
+
+    let words_filtered = words.reduce( (acc,curr, i)=>{
+      const regex = new RegExp(`\s^[${lettersJoined}]+$\s`, "g"); 
+      
+      if(i<40){
+        console.log(regex.test(curr), curr, lettersJoined.toLowerCase())
+      }
+
+      if(regex.test(curr)) acc.push(curr);
+
+      return acc;
+    },[]);
     
+    console.log(">>>", words_filtered.length, words_filtered, lettersJoined);
+
     document.querySelectorAll(".hex").forEach((e,i)=>{
         e.addEventListener("click", addLetter);
         e.querySelector("polygon").setAttribute("data-letter", letters[i]);
         e.querySelector("text").innerHTML = letters[i];
-        
+    });
+
+    document.querySelector("#buttons_container").addEventListener("click", (e)=>{
+      console.log(e.target)
+      if(e.target==="delete"){
+
+      }else if(e.target==="submit"){
+
+      }
+      updateState();
     })
 
     function addLetter(e){
-        console.log(e.target.dataset["letter"])
+      guessArray.push(e.target.dataset["letter"]);
+      updateState();
     }
 
-    console.log("LETTERS: ", letters);
+    function updateState(){
+      document.querySelector("#text").innerText = guessArray.join("");
+    }
 }
